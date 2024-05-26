01dd3b88906ecadc8cf1a2a9bfbf2c1d4a774ec9
Shubham Shukla <shukla111shubh@gmail.com>
2024-05-26 17:40:45 +0530

    Added Coming soon page

---
+++ src/app/comingsoon/page.tsx
@@ -0,0 +1,11 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Comingsoon() {
+	return (
+		<div className="flex h-full flex-col gap-3 justify-center items-center">
+			<h1 className="font-cal text-6xl text-center ">COMING SOON</h1>
+			<Link href="/" className={buttonVariants({ variant: "default" })}>Home</Link>
+		</div>
+	)
+}
